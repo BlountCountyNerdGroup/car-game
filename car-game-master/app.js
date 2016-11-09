904021358ec60288f80e9267aa8d6460859559d5
Kaleb Davenport <kaleb@wowyourclient.com>
2016-11-09 08:39:55 -0500

    Add files via upload

---
+++ car-game-master/app.js
@@ -0,0 +1,39 @@
+// Create a canvas
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+// Road Background
+var bgImage = new Image();
+bgImage.src = 'https://19smitgr.github.io/images/road.png';
+
+// Car image
+var carImage = new Image();
+carImage.src = 'https://19smitgr.github.io/images/car.png';
+
+// Identify the location of the image
+var scrolledTo = 0;
+
+// Main game loop (Note: height of background is 600px)
+setInterval(function() {
+  if (scrolledTo == 600) scrolledTo = 0;
+  scrolledTo += 10;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // For permanent scrolling of the road
+  ctx.drawImage(bgImage, -175, scrolledTo);
+  ctx.drawImage(bgImage, -175, scrolledTo-599);
+
+  ctx.drawImage(carImage, 150, 250, 70, 100);  // Note that last two values are width and height
+}, 20);
+
+// Questions
+var questions = ["Should you try to juggle, ride a unicycle, and drive at the same time?", "Should you let your dog drive your car?", "Are you a good driver?"];
+var answer = ["yes", "no", "maybe"];
+var questionNumber = 0;
+
+setInterval(function() {
+  var response = prompt(questions[questionNumber]);
+
+  if (response != answer[questionNumber]) window.location = 'https://19smitgr.github.io/images/explosion.gif';
+  questionNumber += 1;
+}, 5000);
